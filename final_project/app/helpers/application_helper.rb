96b0d7c1dc395230af036b52861ba38a61aad937
Kevin E. Bauer <kevin.e.bauer@gmail.com>
2015-06-02 16:29:43 -0400

    All Devise customization, Add Facebook with OmniAuth, toastr alerts, updated styling with mobile adapt

--- final_project/app/helpers/application_helper.rb
+++ final_project/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # setting avatar for user account
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  # if user signs up via Facebook, use user image as avatar
+    if user.image
+      user.image
+  # if no facebook login (just email login account), utilize gravitar image
+    else
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=40"
+    end
+  end
 end
---
+++ final_project/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
+
+validates :name, presence: true, length: {maximum: 25}
+
+end
+
+def self.find_for_facebook_oauth(access_token, signed_in_resources=nil)
+  data = access_token.info
+  user = User.where(:provider => access_token.provider, :uid => access_token.uid).first
+
+  if user
+    return user
+  else
+    registered_user = User.where(:email => data.email).first
+    if registered_user
+      return registered_user
+    else
+      user = User.create(
+      name: access_token.raw_info.name,
+      provider: access_token.provider,
+      email: data.email,
+      uid: access_token.uid,
+      image: data.image,
+      password: Devise.friendly_token[0,20],
+      )
+    end
+  end
+end
